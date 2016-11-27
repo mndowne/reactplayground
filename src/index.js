8f69036e797d4e9acbf14e783d9278557f4fc222
mndowne <mndowne@gmail.com>
2016-11-27 16:33:24 -0600

    got it to work, trick is to components

--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class PassedProps extends React.Component{
+    handleChange(e) {
+        var txt = e.target.value;
+        this.props.changePreview(txt);
+    }
+
+    render() {
+        return (
+                <div>
+                    <input 
+                    type="text"  
+                    onChange={this.handleChange.bind(this)}
+                    value={this.props.preview}
+                    />
+                        
+                    <h1>{ this.props.preview }</h1>
+                </div>
+               );
+    }
+}
+
+
 class App extends React.Component{
     constructor() {
         super();
         this.state = {
-field : "hello world"
+            field : "hello world"
         }; 
     }
 
-    handleChange(e) {
-        var txt = e.target.value;
-        this.setState({field: e.target.value});
+    changePreview(preview) {
+        this.setState({field: preview});
     }
 
     render() {
 
         return (
                 <div>
-                <input type="text"  
-                onChange={this.handleChange}
-                value={this.state.field}
-                />
-                <h1>{this.state.field}</h1>
+                    <PassedProps changePreview={ this.changePreview.bind(this) } preview={this.state.field} />
                 </div>
                );
     }
